042dee6e27249ef13377d9edfd2da81dc324ab82
sergiomoratino <60105951+sergiomoratino@users.noreply.github.com>
2022-02-21 16:08:56 +0100

    creating the video controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,10 +6,22 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './public',
+        filename: (req, file, cb) => {
+          const ext = file.mimetype.split('/')[1];
+          cb(null, `${uuidv4()}-${Date.now()}.${ext}`);
+        },
+      })
+    }),
     JwtModule.register({
       secret,
       signOptions: { expiresIn: '2h' },
